b6ab1a06ece6ca929b1073e9bbe656e453a1f9f4
kikogp <fgonzalezp3@alumnos.nebrija.es>
2025-06-28 21:25:11 +0200

    28/06_3

--- main.ts
+++ main.ts
@@ -5,18 +5,19 @@ import { RestaurantModel } from "./types.ts";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers } from "./resolvers.ts";
 
-const MONGO_URL = Deno.env.get("MONGO_URL")
+const MONGO_URL = "mongodb+srv://fgonzalezp3:fgonzalezp3@kikoc.v6p0n.mongodb.net/?retryWrites=true&w=majority&appName=kikoC";//Deno.env.get("MONGO_URL");
 
 if(!MONGO_URL){
-  throw new Error("Mongo url is not defined")
+  throw new Error("Mongo url is not defined");
 }
+
 try{
-const mongoClient = new MongoClient(MONGO_URL)
-await mongoClient.connect
+const mongoClient = new MongoClient(MONGO_URL);
+await mongoClient.connect;
 
-const mongodb = mongoClient.db("ExamenOrdinario")
+const mongodb = mongoClient.db("ExamenOrdinario");
 
-const RestaurantsCollection = mongodb.collection<RestaurantModel>("Restaurantes")
+const RestaurantsCollection = mongodb.collection<RestaurantModel>("Restaurants");
 
 const server = new ApolloServer({
   typeDefs,
@@ -30,5 +31,5 @@ const { url } = await startStandaloneServer(server, {
 console.info(`Server ready at ${url}`);
 
 }catch(e){
-  throw new Error("Fallo al conectarse a la base de datos")
+  throw new Error("Fallo al conectarse a la base de datos");
 }
